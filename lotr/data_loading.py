582f82119e81ce8b13f8cad97c83d08da4debefb
Luigi Petrucco <luigi.petrucco@gmail.com>
2021-08-11 08:36:11 +0200

    moving data from motions-cardinal

---
+++ lotr/data_loading.py
@@ -0,0 +1,52 @@
+from scipy.signal import medfilt, detrend
+import pandas as pd
+import numpy as np
+import tqdm
+
+
+
+def detrend_norm(trace, wnd=3000):
+    """ Normalize trace by dividing it for a super low-pass filtered
+    version of it.
+    """
+    st = pd.Series(trace).rolling(wnd, center=True).mean().values
+
+    # Fill nan values:
+    st[:wnd // 2] = st[wnd // 2]
+    st[-wnd // 2:] = st[-wnd // 2]
+    return trace / st
+
+
+def preprocess_traces(traces, fn, smooth_wnd_s=5, detrend_wnd_s=800):
+    """Preprocess traces by using the detrend normalization funciton
+    above, smoothing them with a median filter, and zscoring them.
+
+    Parameters
+    ----------
+
+    traces : timepoints x nrois np.array
+        traces
+    fn : int
+        sampling frequency
+    smooth_wnd_s : float
+        duration of window for the smoothing (sec)
+    detrend_wnd_s : float
+        duration of window for the detrending (sec)
+
+
+    Returns
+    -------
+    timepoints x nrois np.array
+        Filtered traces
+
+
+    """
+    traces_sm = traces.copy()
+    for i in tqdm(range(traces_sm.shape[1])):
+        traces_sm[:, i] = medfilt(traces[:, i], int(fn * smooth_wnd_s))
+        if detrend_wnd_s is not None:
+            traces_sm[:, i] = detrend_norm(traces_sm[:, i], wnd=int(fn * detrend_wnd_s))
+    if detrend_wnd_s is None:
+        traces_sm = detrend(traces_sm, axis=1)
+
+    return (traces_sm - np.nanmean(traces_sm, 0)) / np.nanstd(traces_sm, 0)
---
+++ lotr/utils.py
@@ -0,0 +1,43 @@
+import numpy as np
+from scipy.interpolate import interp1d
+
+
+def zscore(array):
+    """Nan zscoring function.
+    """
+    return (array - np.nanmean(array))/np.nanstd(array)
+
+
+def interpolate(source_x, source_y, target_x):
+    return interp1d(source_x, source_y, fill_value="extrapolate")(target_x)
+
+
+def pearson_regressors(traces, regressors):
+    """ Gives the pearson correlation coefficient
+
+    :param traces: the traces, with time in rows
+    :param regressors: the regressors, with time in rows
+    :return: the pearson correlation coefficient
+    """
+    # two versions, depending whether there is one or multiple regressors
+    X = traces
+    Y = regressors
+    if len(Y.shape) == 1:
+        numerator = np.dot(X.T, Y) - X.shape[0] * np.nanmean(X, 0) * np.nanmean(Y)
+        denominator = (X.shape[0] - 1) * np.nanstd(X, 0) * np.nanstd(Y)
+        result = numerator / denominator
+    else:
+        numerator = np.dot(X.T, Y) - X.shape[0] * np.outer(np.nanmean(X, 0),
+                                                           np.nanmean(Y, 0))
+        denominator = (X.shape[0] - 1) * np.outer(np.nanstd(X, 0),
+                                                  np.nanstd(Y, 0))
+        result = (numerator / denominator).T
+
+    return result
+
+
+def linear_regression(X, Y):
+    """Get slope and intercept of linear regression between two vectors.
+    """
+    X_mat=np.vstack((np.ones(len(X)), X)).T
+    return np.linalg.inv(X_mat.T.dot(X_mat)).dot(X_mat.T).dot(Y)
