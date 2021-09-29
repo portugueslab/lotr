842f0e57895e574e11d0d3697df8b67796ceb365
Luigi Petrucco <luigi.petrucco@gmail.com>
2021-09-29 18:03:48 +0200

    More filesystem cleanup

--- scripts/move_files.py
+++ scripts/move_files.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 from lotr.file_utils import mirror_all_subfolders
 
-master_path = Path("/Volumes/Shared/experiments/E0040_motions_cardinal/batch210908")
-dest_master_path = Path("/Users/luigipetrucco/Desktop/batch_210908")
+master_path = Path("/Volumes/Shared/experiments/E0040_motions_cardinal/batch210922/last")
+dest_master_path = Path("/Users/luigipetrucco/Desktop/batch_210922")
 
 mirror_all_subfolders(
     master_path, dest_master_path,
@@ -11,7 +11,7 @@ mirror_all_subfolders(
             "*behavior_log*",
             "*stimulus_log*",
             "*metadata.json",
-            "data_from_suite2p_unfiltered.h5",
+            # "data_from_suite2p_unfiltered.h5",
         ]
 )
 
