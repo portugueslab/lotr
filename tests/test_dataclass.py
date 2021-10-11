2e4a86989e00c926004c3f8928fe9a05acdfb026
Luigi Petrucco <luigi.petrucco@gmail.com>
2021-10-11 17:21:31 +0200

    Tests on preprocessing

--- tests/test_dataclass.py
+++ tests/test_dataclass.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 
 from lotr import LotrExperiment
 
@@ -9,3 +10,14 @@ from lotr import LotrExperiment
 def test_dataclass_attribs(attrib, res, sample_path):
     exp = LotrExperiment(sample_path)
     assert getattr(exp, attrib) == res
+
+
+@pytest.mark.parametrize(
+    "attrib, res", [("traces", 4.2231445),
+                    ("raw_traces", 104504930.0),
+                    ("rois_stack", 29304664),
+                    ("anatomy_stack", 6460928.294495098)]
+)
+def test_dataclass_array_sums(attrib, res, sample_path):
+    exp = LotrExperiment(sample_path)
+    assert np.allclose(getattr(exp, attrib).sum(), res)
