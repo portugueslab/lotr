710363803840bdb2268a09dbf74ce4ad2e8c2ff3
Luigi Petrucco <luigi.petrucco@gmail.com>
2021-10-17 11:53:51 +0200

    fixing default dataset location for lab pcs

--- lotr/default_vals.py
+++ lotr/default_vals.py
@@ -13,7 +13,7 @@ T_START = 100  # exclude the very first part of the exp, after lightsheet laser 
 
 # Specify where the dataset is stored.
 DATASET_DEFAULT_LOCATION = (
-    r"\\Funes\Shared\experiments\E0040_motions_cardinal\v21\2p\211006_f2"
+    r"\\FUNES\Shared\experiments\E0071_lotr\full_ring"
 )
 
 # Anatomical orientation of source data
