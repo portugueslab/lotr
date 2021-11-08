33f1492bede1fe718c7f20abe537c1664d22f9da
Luigi Petrucco <luigi.petrucco@gmail.com>
2021-11-08 18:02:53 +0100

    utils for file diff

--- scripts/move_files.py
+++ scripts/move_files.py
@@ -2,18 +2,18 @@ from pathlib import Path
 
 from lotr.file_utils import mirror_all_subfolders
 
-master_path = Path(
-    "/Volumes/Shared/experiments/E0040_motions_cardinal/batch210922/last"
+dest_master_path = Path(
+    "/Volumes/Shared/experiments/E0071_lotr/full_ring"
 )
-dest_master_path = Path("/Users/luigipetrucco/Desktop/batch_210922")
+master_path = Path("/Users/luigipetrucco/Desktop/all_source_data/full_ring")
 
 mirror_all_subfolders(
     master_path,
     dest_master_path,
     file_patterns=[
-        "*behavior_log*",
-        "*stimulus_log*",
-        "*metadata.json",
+        "*centering_mtx.npy*",
         # "data_from_suite2p_unfiltered.h5",
     ],
 )
+
+
---
+++ scripts/synch_files_in_mirror.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from tqdm import tqdm
+from shutil import copy
+
+file_patterns = ["*centering_mtx.npy*"]
+overwrite = False
+
+dest_master_path = Path(
+    "/Volumes/Shared/experiments/E0071_lotr/full_ring"
+)
+master_path = Path("/Users/luigipetrucco/Desktop/all_source_data/full_ring")
+
+
+for path in tqdm(list(master_path.glob("*[0-9]_f[0-9]*/*[0-9]_f[0-9]*"))):
+    dest_path = dest_master_path / path.parent.name / path.name
+    for pattern in file_patterns:
+        for file in path.glob(pattern):
+            if overwrite or not (dest_path / file.name).exists():
+                copy(file, dest_path / file.name)
